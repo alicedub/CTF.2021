1c0fef430d7a086d89359ec378281e99e97d1ae2
Antoine Stevan <stevan.antoine@gmail.com>
2021-09-16 12:17:10 +0200

    New proposition for q40.

--- solutions/40.sh
+++ solutions/40.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # How many total times is pandas imported in the transformers source code?
-# Response: 9
+# Response: 40
 
 grep -rnw ./transformers/src/transformers/* -e "import pandas" | wc -l
+ssh ctf 'cd transformers && grep -irm 1 "import pandas" | wc -l'
