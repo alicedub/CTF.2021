918d34b579246f4daf61ee578c4705aeeb982136
Hugo Bourdon <hugo@LAPTOP-SNTO7RDR.localdomain>
2021-09-16 12:16:54 +0200

    proposition modif q31

--- solutions/31.sh
+++ solutions/31.sh
@@ -2,4 +2,4 @@
 #What is the full version of the commit ID of the first commit of the ~/transformers repository?
 #Solution: 43badf217d1ccfaf486e2cbb1b3567226b5e95bf
 
-transformers$ git log | tail
+transformers$ git log --pretty=oneline --pretty=format:"%H" | tail -1
